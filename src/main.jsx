3f87ceaddf407f2bb5a4c976b95f3ea0f8765309
Susana Niroula <susananiroula@Susanas-MacBook-Air.local>
2026-02-20 12:46:59 +0545

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './Router';
+import './index.css';
+import { AdminProvider } from './context/AdminContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AdminProvider>
+      <Router />
+    </AdminProvider>
+  </React.StrictMode>
+);
